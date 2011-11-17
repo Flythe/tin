b44f34f087ee63be2a2e3e1b0681b4948b6ae5d3
allard <Flythe1@gmail.com>
2011-11-17 13:04:02 +0100

    video

--- video.php
+++ video.php
@@ -1,10 +1,10 @@
-<OBJECT id="VIDEO" width="320" height="240"
-	CLASSID="CLSID:6BF52A52-394A-11d3-B153-00C04F79FAA6"
-	type="application/x-oleobject">
-	
-	<PARAM NAME="URL" VALUE="\\172.16.1.33\video\30\3045-1.wmv">
-	<PARAM NAME="SendPlayStateChangeEvents" VALUE="True">
-	<PARAM NAME="AutoStart" VALUE="True">
-	<PARAM name="uiMode" value="none">
-	<PARAM name="PlayCount" value="9999">
-</OBJECT>
+<object id="mediaplayer" classid="clsid:22d6f312-b0f6-11d0-94ab-0080c74c7e95" codebase="http://activex.microsoft.com/activex/controls/mplayer/en/nsmp2inf.cab#version=5,1,52,701" standby="loading microsoft windows media player components..." type="application/x-oleobject" width="320" height="310">
+<param name="filename" value="\\172.16.1.33\video\30\3045-1.wmv">
+     <param name="animationatstart" value="true">
+     <param name="transparentatstart" value="true">
+     <param name="autostart" value="true">
+     <param name="showcontrols" value="true">
+     <param name="ShowStatusBar" value="true">
+     <param name="windowlessvideo" value="true">
+     <embed src="./test.wmv" autostart="true" showcontrols="true" showstatusbar="1" bgcolor="white" width="320" height="310">
+</object>
