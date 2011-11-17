8cf507ba00d7584c9240ee0a4a6fbe883d78a1e8
allard <Flythe1@gmail.com>
2011-11-17 12:59:55 +0100

    video

---
+++ video.php
@@ -0,0 +1,10 @@
+<OBJECT id="VIDEO" width="320" height="240"
+	CLASSID="CLSID:6BF52A52-394A-11d3-B153-00C04F79FAA6"
+	type="application/x-oleobject">
+	
+	<PARAM NAME="URL" VALUE="\\172.16.1.33\video\30\3045-1.wmv">
+	<PARAM NAME="SendPlayStateChangeEvents" VALUE="True">
+	<PARAM NAME="AutoStart" VALUE="True">
+	<PARAM name="uiMode" value="none">
+	<PARAM name="PlayCount" value="9999">
+</OBJECT>
